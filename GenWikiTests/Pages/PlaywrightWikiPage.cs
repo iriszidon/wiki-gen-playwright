6ba56aafb238157dfba7aea539654f80af652cf5
Iris Opper <iriszidon@gmail.com>
2026-02-07 18:15:18 +0200

    Add main page

---
+++ GenWikiTests/Pages/PlaywrightWikiPage.cs
@@ -0,0 +1,13 @@
+using Microsoft.Playwright;
+using System.Threading.Tasks;
+
+namespace GenWikiTests.Pages;
+
+public class PlaywrightWikiPage : BasePage
+{
+    public PlaywrightWikiPage(IPage page) : base(page)
+    {
+
+    }
+
+}
