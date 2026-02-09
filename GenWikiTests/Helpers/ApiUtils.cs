a70c92e1197a3c3de1af84a40d7f62c134ccdc31
Iris Opper <iriszidon@gmail.com>
2026-02-09 13:54:05 +0200

    Add wiki text object

--- GenWikiTests/Helpers/ApiUtils.cs
+++ GenWikiTests/Helpers/ApiUtils.cs
@@ -1,4 +1,5 @@
 using DotNetEnv;
+using System.Text.Json.Serialization;
 
 namespace GenWikiTests.Helpers
 {
@@ -6,6 +7,8 @@ namespace GenWikiTests.Helpers
     {
         static string BASE_URL = Environment.GetEnvironmentVariable("API_URL");
         private static string WIKI_USER = Environment.GetEnvironmentVariable("WIKI_USER");
+
+        //Get rid of this method
         public static async Task<string> GetDebuggingFeaturesTextAsync(string format, string pageTitle, int sectionNumber, CancellationToken cancellationToken = default)
         {
             LoadEnvVariables();
@@ -23,12 +26,52 @@ namespace GenWikiTests.Helpers
             return content;
         }
 
+        public static async Task<string> GetDebuggingFeaturesWikiTextAsync(string format, string pageTitle, int sectionNumber, string textProp = "wikitext", CancellationToken cancellationToken = default)
+        {
+            LoadEnvVariables();
+            var client = new HttpClient();
+            // Per Wikimedia policy: include identifying UA with contact URL/email
+            client.DefaultRequestHeaders.UserAgent.ParseAdd(WIKI_USER);
+            HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, $"{BASE_URL}?action=parse&format={format}&formatversion=2&page={pageTitle}&prop={textProp}&section={sectionNumber}");
+
+            Console.WriteLine($"Request URL:\n ---- {request.RequestUri}");
+            var response = await client.SendAsync(request);
+
+            response.EnsureSuccessStatusCode();
+            string content = await response.Content.ReadAsStringAsync();
+            ParseResponse apiResponse = System.Text.Json.JsonSerializer.Deserialize<ParseResponse>(content);
+            string wikiText = apiResponse.Parse.WikiText;
+            Console.WriteLine(content);
+            Console.WriteLine($"Extracted wikitext:\n{wikiText}");
+            return wikiText;
+        }
+
         public static void LoadEnvVariables()
         {
             string envFileDirectory = DirectoryUtils.GetNthParentDirectory(4);
             Console.WriteLine($"envFileDirectory ------> : {envFileDirectory}");
             Env.Load($"{envFileDirectory}\\.env"); // loads .env from project root
         }
+    }
+
+
+
+    public class ParseResponse
+    {
+        [JsonPropertyName("parse")]
+        public ParseData Parse { get; set; }
+    }
+
+    public class ParseData
+    {
+        [JsonPropertyName("title")]
+        public string Title { get; set; }
+
+        [JsonPropertyName("pageid")]
+        public int PageId { get; set; }
+
+        [JsonPropertyName("wikitext")]
+        public string WikiText { get; set; }
     }
 }
 
