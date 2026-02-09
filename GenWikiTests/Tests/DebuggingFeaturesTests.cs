e9943ae9f755364540e939055ea8ce0532303877
Iris Opper <iriszidon@gmail.com>
2026-02-09 15:07:15 +0200

    Add allure

--- GenWikiTests/Tests/DebuggingFeaturesTests.cs
+++ GenWikiTests/Tests/DebuggingFeaturesTests.cs
@@ -1,55 +1,58 @@
 using GenWikiTests.Pages;
 using GenWikiTests.Helpers;
-namespace GenWikiTests.Tests;
+using Allure.Commons;
 
 
-[TestClass]
-public class ExampleTest : BaseTest
+namespace GenWikiTests.Tests
 {
-    [TestMethod]
-    [TestCategory("WordCount")]
-    [DescriptionAttribute("Count unique words using UI and API and compare results")]
-    public async Task GetTextFromUITest()
+    [TestClass]
+    public class ExampleTest : BaseTest
     {
-        //Todo: Move this to before test setup and reuse page object across tests in the class
-        var page = await _browser.NewPageAsync();
-        var wikiPlaywrightMainPage = new PlaywrightWikiPage(page);
-        await wikiPlaywrightMainPage.NavigateAsync();
+        [TestMethod]
+        [TestCategory("WordCount")]
+        [DescriptionAttribute("Count unique words using UI and API and compare results")]
+        public async Task GetTextFromUITest()
+        {
+            //Todo: Move this to before test setup and reuse page object across tests in the class
+            var page = await _browser.NewPageAsync();
+            var wikiPlaywrightMainPage = new PlaywrightWikiPage(page);
+            await wikiPlaywrightMainPage.NavigateAsync();
 
-        await wikiPlaywrightMainPage.ClickDebuggingFeaturesAsync();
-        DebuggingFeaturesSectionPage debuggingFeaturesSectionPage = new DebuggingFeaturesSectionPage(page);
-        string uiSectionText = await debuggingFeaturesSectionPage.GetNormalizedTextAsync();
-        Console.WriteLine(TextUtils.CountUniqueWords(uiSectionText));
-        // Get the text using Wki Parse API 
-        string apiSectionText = await ApiUtils.GetDebuggingFeaturesWikiTextAsync("json", "Playwright_(software)", 5);
-        string sectionTextWithoutWikiRef = TextUtils.RemoveWikiReferenceToolTip(apiSectionText, "ref");
-        string normalizedSectionText = TextUtils.NormalizeTextWithCharsOnly(sectionTextWithoutWikiRef);
-        // Compare the results
-        HashSet<string> uiUniqueWordCount = TextUtils.CountUniqueWords(uiSectionText);
-        HashSet<string> apiUniqueWordCount = TextUtils.CountUniqueWords(normalizedSectionText);
-        Assert.AreEqual(uiUniqueWordCount.Count, apiUniqueWordCount.Count, "Unique word count should match between UI and API results.");
-    }
-
-
-    [TestMethod]
-    [TestCategory("Wikitext")]
-    public async Task GetWikiTextFromApiTest()
-    {
-        string sectionText = await ApiUtils.GetDebuggingFeaturesWikiTextAsync("json", "Playwright_(software)", 5);
-        string sectionTextWithoutWikiRef = TextUtils.RemoveWikiReferenceToolTip(sectionText, "ref");
-        string normalizedSectionText = TextUtils.NormalizeTextWithCharsOnly(sectionTextWithoutWikiRef);
-        Console.WriteLine($"Original wikitext from API: \n{sectionText}");
-        Console.WriteLine($"Normalized wikitext from API: \n{normalizedSectionText}");
-        Assert.IsFalse(string.IsNullOrEmpty(normalizedSectionText), "Section text from API should not be empty.");
-    }
+            await wikiPlaywrightMainPage.ClickDebuggingFeaturesAsync();
+            DebuggingFeaturesSectionPage debuggingFeaturesSectionPage = new DebuggingFeaturesSectionPage(page);
+            string uiSectionText = await debuggingFeaturesSectionPage.GetNormalizedTextAsync();
+            Console.WriteLine(TextUtils.CountUniqueWords(uiSectionText));
+            // Get the text using Wki Parse API 
+            string apiSectionText = await ApiUtils.GetDebuggingFeaturesWikiTextAsync("json", "Playwright_(software)", 5);
+            string sectionTextWithoutWikiRef = TextUtils.RemoveWikiReferenceToolTip(apiSectionText, "ref");
+            string normalizedSectionText = TextUtils.NormalizeTextWithCharsOnly(sectionTextWithoutWikiRef);
+            // Compare the results
+            HashSet<string> uiUniqueWordCount = TextUtils.CountUniqueWords(uiSectionText);
+            HashSet<string> apiUniqueWordCount = TextUtils.CountUniqueWords(normalizedSectionText);
+            Assert.AreEqual(uiUniqueWordCount.Count, apiUniqueWordCount.Count, "Unique word count should match between UI and API results.");
+        }
 
 
-    [TestMethod]
-    public void CountUniqueWords_Test()
-    {
-        string input = "Hello world Hello everyone Welcome to the world of testing".ToLower();
-        HashSet<string> uniqueWordCount = TextUtils.CountUniqueWords(input);
-        Console.WriteLine($"Unique word count: {uniqueWordCount.Count}");
-        Assert.AreEqual(8, uniqueWordCount.Count, "Unique word count should be 8.");
+        [TestMethod]
+        [TestCategory("Wikitext")]
+        public async Task GetWikiTextFromApiTest()
+        {
+            string sectionText = await ApiUtils.GetDebuggingFeaturesWikiTextAsync("json", "Playwright_(software)", 5);
+            string sectionTextWithoutWikiRef = TextUtils.RemoveWikiReferenceToolTip(sectionText, "ref");
+            string normalizedSectionText = TextUtils.NormalizeTextWithCharsOnly(sectionTextWithoutWikiRef);
+            Console.WriteLine($"Original wikitext from API: \n{sectionText}");
+            Console.WriteLine($"Normalized wikitext from API: \n{normalizedSectionText}");
+            Assert.IsFalse(string.IsNullOrEmpty(normalizedSectionText), "Section text from API should not be empty.");
+        }
+
+
+        [TestMethod]
+        public void CountUniqueWords_Test()
+        {
+            string input = "Hello world Hello everyone Welcome to the world of testing".ToLower();
+            HashSet<string> uniqueWordCount = TextUtils.CountUniqueWords(input);
+            Console.WriteLine($"Unique word count: {uniqueWordCount.Count}");
+            Assert.AreEqual(8, uniqueWordCount.Count, "Unique word count should be 8.");
+        }
     }
 }
