449bf2d7ffe7b0e3891eef642a65dad3cff240af
Iris Opper <iriszidon@gmail.com>
2026-02-11 08:02:34 +0200

    Remove unneeded tests

--- GenWikiTests/Tests/DebuggingFeaturesTests.cs
+++ GenWikiTests/Tests/DebuggingFeaturesTests.cs
@@ -27,28 +27,5 @@ namespace GenWikiTests.Tests
             HashSet<string> apiUniqueWordCount = TextUtils.CountUniqueWords(normalizedSectionText);
             Assert.AreEqual(uiUniqueWordCount.Count, apiUniqueWordCount.Count, "Unique word count should match between UI and API results.");
         }
-
-
-        [TestMethod]
-        [TestCategory("Wikitext")]
-        public async Task GetWikiTextFromApiTest()
-        {
-            string sectionText = await ApiUtils.GetDebuggingFeaturesWikiTextAsync("json", "Playwright_(software)", 5);
-            string sectionTextWithoutWikiRef = TextUtils.RemoveWikiReferenceToolTip(sectionText, "ref");
-            string normalizedSectionText = TextUtils.NormalizeTextWithCharsOnly(sectionTextWithoutWikiRef);
-            Console.WriteLine($"Original wikitext from API: \n{sectionText}");
-            Console.WriteLine($"Normalized wikitext from API: \n{normalizedSectionText}");
-            Assert.IsFalse(string.IsNullOrEmpty(normalizedSectionText), "Section text from API should not be empty.");
-        }
-
-
-        [TestMethod]
-        public void CountUniqueWords_Test()
-        {
-            string input = "Hello world Hello everyone Welcome to the world of testing".ToLower();
-            HashSet<string> uniqueWordCount = TextUtils.CountUniqueWords(input);
-            Console.WriteLine($"Unique word count: {uniqueWordCount.Count}");
-            Assert.AreEqual(8, uniqueWordCount.Count, "Unique word count should be 8.");
-        }
     }
 }
